2fa19a5b7afb98b2acb2530c701b8d2bee6a35c1
Josh Chen <joshchen@princeton.edu>
2013-12-08 03:37:41 -0500

    Added ctl generator to process.py. Updated bins+makefile.

--- jlj/process.py
+++ jlj/process.py
@@ -62,8 +62,16 @@ def run_phyml(fname_aln, n_bootstrap):
     return fname_tree
 
 
-def make_ctl(fname_nuc):
-    sys.stderr.write("\nSTEP: make_ctl(%s)\n" % fname_nuc)
+def make_ctl(fname_nuc, fname_tree):
+    sys.stderr.write("\nSTEP: make_ctl(%s, %s)\n" % (fname_nuc, fname_tree)
+    fname_ctl = ".".join(fname_nuc.split(".")[:-1]) + ".ctl"
+    with open(fname_nuc, "w") as fw:
+        with open("codonml.ctl", "r") as fr:
+            line = fr.readline()
+            fw.write(fr.readline() % fname_nuc)
+            fw.write(fr.readline() % fname_tree)
+            for line in fr:
+                fw.write(line)
     return fname_ctl
 
 
@@ -78,19 +86,29 @@ def run_codeml(fname_ctl):
     # TODO: redirect output into more sensible format
 
 
-if __name__ == "__main__":
+def main():
     # take protein aln file in CSA format from caprasingh08 dataset
     parser = argparse.ArgumentParser(description="produce dn/ds output from simple aln file")
     parser.add_argument('-n', dest='n_bootstrap', type=int, default=1,
         help="number of bootstrapped trees")
+    parser.add_argument('-o', dest='out_dir', default="out",
+        help="directory for output")
     parser.add_argument('fname_aln')
     args = parser.parse_args()
 
+    current_dir = os.getcwd()
+
     fname_aln = args.fname_aln
     n_bootstrap = args.n_bootstrap
+    out_dir = args.out_dir
 
+    # TODO: the preprocessing can kind of be done in parallel
     fname_nuc = fetch_nuc(fname_aln)
     fname_codon = run_pal2nal(fname_aln, fname_nuc)
     fname_tre = run_phyml(fname_aln, n_bootstrap)
     fname_ctl = make_ctl(fname_codon, fname_tree)
     run_codeml(fname_ctl)
+
+
+if __name__ == "__main__":
+    main()
