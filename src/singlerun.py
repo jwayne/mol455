8300569fc5a1bccd3681ff8aff4ab4b08c46aebd
Josh Chen <joshchen@princeton.edu>
2013-12-09 03:31:23 -0500

    fixed stupid errors in preprocessing

--- src/singlerun.py
+++ src/singlerun.py
@@ -7,7 +7,7 @@ import os
 from preprocess import (fetch_homologs, fetch_seqs, run_clustalw2, run_pal2nal,
                         run_phyml, convert_maintree, convert_boottrees,
                         make_ctl)
-from utils import ts_str
+from utils import ts_str, bin_dir
 
 
 AUTOMATIC = "AUTOMATIC"
@@ -24,20 +24,33 @@ class SingleRun(object):
             absolute paths of input files
         """
         inputs = ('homolog_gbids',
-                  'homolog_nucs', 'homolog_aas', 'aa_aln',
-                  'codon_aln',
+                  'homolog_nucs', 'homolog_aas',
+                  'aa_aln', 'codon_aln',
                   'tree', 'boot_trees',
                   'ctl_template')
         inputs = dict((inp, None) for inp in inputs)
 
+        fnames_in = [os.path.abspath(fname_in) for fname_in in fnames_in]
         for fname_in in fnames_in:
             # Use filename to determine what input type it is
             # TODO: Open up file to sanity check inputs, and provide greater flexibility
-            if fname_in.endswith(".gibds"):
+            fname_part = os.path.split(fname_in)[-1]
+            if fname_in.endswith(".gbids"):
                 if inputs['homolog_gbids']:
                     raise ValueError("Duplicate lists of gbids")
                 inputs['homolog_gbids'] = fname_in
-            # TODO: homolog_nucs, homolog_aas, aa_aln
+            elif fname_part == "nucleotides.fasta":
+                if inputs['homolog_nucs']:
+                    raise ValueError("Duplicate nucleotide sequence files")
+                inputs['homolog_nucs'] = fname_in
+            elif fname_part == "protein.fasta":
+                if inputs['homolog_aas']:
+                    raise ValueError("Duplicate protein sequence files")
+                inputs['homolog_aas'] = fname_in
+            elif fname_part == "protein.aln":
+                if inputs['aa_aln']:
+                    raise ValueError("Duplicate protein alignment files")
+                inputs['aa_aln'] = fname_in
             elif fname_in.endswith(".aln"):
                 if inputs['codon_aln']:
                     raise ValueError("Duplicate codon alignment files")
@@ -70,12 +83,11 @@ class SingleRun(object):
                 # If no tree provided, default to not analyzing bootstrap trees
                 analyze_bootstrap = False
 
-        fnames_in = [os.abspath(fname_in) for fname_in in fnames_in]
         if not dirname_out:
             head, tail = os.path.split(fnames_in[0])
             os.chdir(head)
             dirname_out = ".".join(tail.split(".")[:-1]) + ts_str()
-            dirname_out = os.abspath(dirname_out)
+            dirname_out = os.path.abspath(dirname_out)
             if os.path.exists(dirname_out):
                 raise NotImplementedError()
             os.mkdir(dirname_out)
@@ -116,7 +128,7 @@ class SingleRun(object):
                     self.inputs['homolog_nucs'], a = fetch_seqs(self.inputs['homolog_gbids'])
                     if self.inputs['homolog_aas']:
                         sys.stderr.write("Overwriting input homolog_aas\n")
-                        self.inputs['homolog_aas'] = a
+                    self.inputs['homolog_aas'] = a
         def _check_homolog_aas():
             if not self.inputs['homolog_aas']:
                 if not self.inputs['homolog_gbids']:
@@ -128,11 +140,11 @@ class SingleRun(object):
                     a, self.inputs['homolog_aas'] = fetch_seqs(self.inputs['homolog_gbids'])
                     if self.inputs['homolog_nucs']:
                         sys.stderr.write("Overwriting input homolog_nucs\n")
-                        self.inputs['homolog_nucs'] = a
+                    self.inputs['homolog_nucs'] = a
         def _check_aa_aln():
             if not self.inputs['aa_aln']:
                 _check_homolog_aas()
-                self.inputs['aa_aln'] = run_clustalw(self.inputs['homolog_aas'])
+                self.inputs['aa_aln'] = run_clustalw2(self.inputs['homolog_aas'])
         def _check_codon_aln():
             if not self.inputs['codon_aln']:
                 _check_homolog_nucs()
@@ -171,7 +183,7 @@ class SingleRun(object):
 
     def process(self):
         print "Run the following--"
-        print "chdir %s" % self.dirname_out
+        print "cd %s" % self.dirname_out
         print "%s/../idea-2.5.1/idea" % bin_dir()
         print "Then go to File > Load configuration > codonml.ctl"
         print "I think."
@@ -186,12 +198,12 @@ def main():
         help="output directory. Defaults to new directory in folder of first positional argument")
     parser.add_argument('-n', dest='n_bootstrap', type=int, default=1,
         help="number of bootstrapped trees. Defaults to 1")
-    parser.add_argument('-b', dest='analyze_boostrap', action='store_true', default=AUTOMATIC,
+    parser.add_argument('-b', dest='analyze_bootstrap', action='store_true', default=AUTOMATIC,
         help="run codeML on all bootstrap trees, and analyze the resulting dnds distribution per site")
     args = parser.parse_args()
     
-    singlerun = SingleRun(args.fnames_in, args.dirname_out)
-    singlerun.preprocess(args.n_boostrap, args.analyze_boostrap)
+    singlerun = SingleRun(args.fnames_in, args.dirname_out, args.analyze_bootstrap)
+    singlerun.preprocess(args.n_bootstrap, args.analyze_bootstrap)
     singlerun.process()
 
 
